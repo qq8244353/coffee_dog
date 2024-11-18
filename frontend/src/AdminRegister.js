842d39b578c402832c0425d0442e4e0176539558
qq8244353 <qqq000a@outlook.com>
2024-11-19 00:21:02 +0900

    modal作成

--- frontend/src/AdminRegister.js
+++ frontend/src/AdminRegister.js
@@ -14,75 +14,122 @@ function AdminRegister() {
   const [lemonade, setLemonade] = useState(0)
   const [mapleMadeleine, setMapleMadeleine] = useState(0)
   const [amandeChocolat, setAmandeChocolat] = useState(0)
+  const [modal, setModal] = useState(false)
+  const [saleId, setSaleId] = useState(null)
   return (
-    <Flex
-      containerCss={css`
-        flex-direction: column;
-      `}
-    >
-      <AdminInput value={hotCoffee} setValue={setHotCoffee} name="ホットコーヒー" />
-      <AdminInput value={iceCoffee} setValue={setIceCoffee} name="アイスコーヒー" />
-      <AdminInput value={specialHotCoffee} setValue={setSpecialHotCoffee} name="ホットコーヒー + オプション" />
-      <AdminInput value={specialIceCoffee} setValue={setSpecialIceCoffee} name="アイスコーヒー + オプション" />
-      <AdminInput value={lemonade} setValue={setLemonade} name="レモネード" />
-      <AdminInput value={mapleMadeleine} setValue={setMapleMadeleine} name="メイプルマドレーヌ" />
-      <AdminInput value={amandeChocolat} setValue={setAmandeChocolat} name="アマンドショコラ" />
-      <span>
-        合計金額 
-        {
-          (hotCoffee + iceCoffee) * 250 +
-          (specialHotCoffee + specialIceCoffee + lemonade) * 300 +
-          (mapleMadeleine + amandeChocolat) * 220
-        } 円
-      </span>
-      <button onClick={() => {
-        let items = []
-        if (hotCoffee > 0) {
-          items.push({item_id: 10, cnt: hotCoffee })
-        }
-        if (iceCoffee > 0) {
-          items.push({item_id: 11, cnt: iceCoffee })
-        }
-        if (specialHotCoffee > 0) {
-          items.push({item_id: 12, cnt: specialHotCoffee })
-        }
-        if (specialIceCoffee > 0) {
-          items.push({item_id: 13, cnt: specialIceCoffee })
-        }
-        if (lemonade > 0) {
-          items.push({item_id: 20, cnt: lemonade })
-        }
-        if (mapleMadeleine > 0) {
-          items.push({item_id: 30, cnt: mapleMadeleine })
-        }
-        if (amandeChocolat > 0) {
-          items.push({item_id: 31, cnt: amandeChocolat })
-        }
-        fetch('http://127.0.0.1:1324/admin-orders', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            items: items,
-            register_person_id: 0,
-          })
-        })
-        .then(res => {
-          if (res.ok) {
-            return res.json()
-          } else {
-              console.log(res.text())
+    <>
+      <Flex
+        containerCss={css`
+          flex-direction: column;
+        `}
+      >
+        <AdminInput value={hotCoffee} setValue={setHotCoffee} name="ホットコーヒー" />
+        <AdminInput value={iceCoffee} setValue={setIceCoffee} name="アイスコーヒー" />
+        <AdminInput value={specialHotCoffee} setValue={setSpecialHotCoffee} name="ホットコーヒー + オプション" />
+        <AdminInput value={specialIceCoffee} setValue={setSpecialIceCoffee} name="アイスコーヒー + オプション" />
+        <AdminInput value={lemonade} setValue={setLemonade} name="レモネード" />
+        <AdminInput value={mapleMadeleine} setValue={setMapleMadeleine} name="メイプルマドレーヌ" />
+        <AdminInput value={amandeChocolat} setValue={setAmandeChocolat} name="アマンドショコラ" />
+        <span>
+          合計金額 
+          {
+            (hotCoffee + iceCoffee) * 250 +
+            (specialHotCoffee + specialIceCoffee + lemonade) * 300 +
+            (mapleMadeleine + amandeChocolat) * 220
+          } 円
+        </span>
+        <button onClick={() => {
+          let items = []
+          if (hotCoffee > 0) {
+            items.push({item_id: 10, cnt: hotCoffee })
           }
-        })
-        .then(data => {
-            console.log(data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      }}>登録</button>
-    </Flex>
+          if (iceCoffee > 0) {
+            items.push({item_id: 11, cnt: iceCoffee })
+          }
+          if (specialHotCoffee > 0) {
+            items.push({item_id: 12, cnt: specialHotCoffee })
+          }
+          if (specialIceCoffee > 0) {
+            items.push({item_id: 13, cnt: specialIceCoffee })
+          }
+          if (lemonade > 0) {
+            items.push({item_id: 20, cnt: lemonade })
+          }
+          if (mapleMadeleine > 0) {
+            items.push({item_id: 30, cnt: mapleMadeleine })
+          }
+          if (amandeChocolat > 0) {
+            items.push({item_id: 31, cnt: amandeChocolat })
+          }
+          fetch('http://127.0.0.1:1324/admin-orders', {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              items: items,
+              register_person_id: 0,
+            })
+          })
+          .then(res => {
+            if (res.ok) {
+              return res.json()
+            } else {
+              console.log(res.text())
+            }
+          })
+          .then(saleId=> {
+            setModal(true)
+            setSaleId(saleId)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        }}>登録</button>
+      </Flex>
+      {modal && (
+        <div
+          css={css`
+          position:fixed;
+          top:0;
+          left:0;
+          width:100%;
+          height:100%;
+          background-color:rgba(0,0,0,0.5);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          `}
+        >
+          <Flex
+            containerCss={css`
+              background-color: white;
+              width: 20em;
+              height: 20em;
+              border-radius: 5em;
+              align-items: center;
+              justify-content: center;
+              flex-direction: column;
+            `}
+          >
+            <span>{saleId}</span>
+            <button onClick={() => {
+              setHotCoffee(0)
+              setIceCoffee(0)
+              setSpecialHotCoffee(0)
+              setSpecialIceCoffee(0)
+              setLemonade(0)
+              setMapleMadeleine(0)
+              setAmandeChocolat(0)
+              setModal(false)
+              setSaleId(null)
+            }}>
+              閉じる
+            </button>
+          </Flex>
+        </div>
+      )}
+    </>
   );
 }
 
